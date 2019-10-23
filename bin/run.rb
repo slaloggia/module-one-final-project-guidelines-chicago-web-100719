736c9344732354d213d8212c622b52fc81a5a1ee
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-10-23 17:01:54 -0500

    added line spaces to run file for readability in CLI

--- bin/run.rb
+++ bin/run.rb
@@ -4,6 +4,7 @@ require_relative '../config/environment'
 
 def welcome 
     puts "Welcome to SuperWorld! Enter your name:"
+    puts "\n"
     gets.chomp
 end
 
@@ -16,16 +17,19 @@ def set_user(name)
 end
  
 def options
+    puts "***MENU***"
     puts "-find : Find a character in the database"
     puts "-add : Add a character to your favorites"
     puts "-delete : Remove character from your favorites"
     puts "-rate : Add/Update character ratings"
     puts "-favorites : See your favorite characters"
     puts "-publisher : Finds your favorite publisher"
-    puts "-strongest: See your strongest character"
-    puts "-fastest: See your fastest character"
-    puts "-smartest: See your smartest character"
-    puts "-exit: Exits the program"
+    puts "-strongest : See your strongest character"
+    puts "-fastest : See your fastest character"
+    puts "-smartest : See your smartest character"
+    puts "-menu : View this menu"
+    puts "-exit : Exits the program" 
+    puts "\n"
 end
  
  
@@ -40,41 +44,44 @@ def runner
     name = welcome
     user = set_user(name)
     p user
+    puts "\n"
     user_command = nil
+    options
     until user_command == "exit"
-        puts "What do you want to do???"
-        options
+        puts "What do you want to do???\n"
         user_command = gets.strip.downcase
     case user_command
         when "find"
         name = get_user_input.split.map(&:capitalize).join(' ')
         character = Character.find_by(name: name)
-        p character
+        p character 
         when "add"
         name = get_user_input.split.map(&:capitalize).join(' ')
         user.add_character_to_favorites(name)
+        puts "\n"
         when "delete"
         name = get_user_input.split.map(&:capitalize).join(' ')
         user.delete_from_favorites(name)
+        puts "\n"
         when "rate"
         name = get_user_input.split.map(&:capitalize).join(' ')
-        puts "Enter a rating for this character"
+        puts "Enter a rating for this character \n"
         rating = gets.chomp
         user.update_character_rating(name, rating)
+        puts "\n"
         when "favorites"
         user.favorite_characters_names  
+        puts "\n"
         when "strongest"
         p user.strongest
-
         when "smartest"
         p user.smartest
-        
         when "fastest"
         p user.fastest
-
         when "publisher"
             user.favorite_publishers
-
+        when "menu"
+            options
         when "exit"
             exit_app
         end
