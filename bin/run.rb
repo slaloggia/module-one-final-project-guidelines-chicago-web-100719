97126b09061bbe112668bbae8f6cc8b83288742e
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-10-23 15:37:18 -0500

    added program loop and exit method

--- bin/run.rb
+++ bin/run.rb
@@ -5,67 +5,79 @@ require_relative '../config/environment'
 def welcome 
     puts "Welcome to SuperWorld! Enter your name:"
     gets.chomp
- end
+end
+
+def exit_app
+    puts "Until next time...EXCELSIOR!!!"
+end
  
- def set_user(name)
-     User.find_or_create_by(name: name)
- end
+def set_user(name)
+    User.find_or_create_by(name: name)
+end
  
- def options
-     puts "-find : Find a character in the database"
-     puts "-add : Add a character to your favorites"
-     puts "-delete : Remove character from your favorites"
-     puts "-rate : Add/Update character ratings"
-     puts "-favorites : See your favorite characters"
-     puts "-strongest: See your strongest character"
-     puts "-fastest: See your fastest character"
-     puts "-smartest: See your smartest character"
- end
+def options
+    puts "-find : Find a character in the database"
+    puts "-add : Add a character to your favorites"
+    puts "-delete : Remove character from your favorites"
+    puts "-rate : Add/Update character ratings"
+    puts "-favorites : See your favorite characters"
+    puts "-strongest: See your strongest character"
+    puts "-fastest: See your fastest character"
+    puts "-smartest: See your smartest character"
+    puts "-exit: Exits the program"
+end
+ 
  
      
  
- def get_user_input
-     puts "Enter a character name:"
-     user_input = gets.strip
- end
+def get_user_input
+    puts "Enter a character name:"
+    user_input = gets.strip
+end
  
- def runner
-     name = welcome
-     user = set_user(name)
-     p user
-     puts "What do you want to do???"
-     options
-     user_command = gets.strip.downcase
-     case user_command
-         when "find"
-            name = get_user_input
-            character = Character.find_by(name: name)
-            p character
-         when "add"
-            name = get_user_input
-            user.add_character_to_favorites(name)
-         when "delete"
-            name = get_user_input
-            user.delete_from_favorites(name)
-         when "rate"
-            name = get_user_input
-            puts "Enter a rating for this character"
-            rating = gets.chomp
-            user.update_character_rating(name, rating)
-         when "favorites"
-            user.favorite_characters_names  
-         when "strongest"
-            p user.strongest
+def runner
+    name = welcome
+    user = set_user(name)
+    p user
+    user_command = nil
+    until user_command == "exit"
+        puts "What do you want to do???"
+        options
+        user_command = gets.strip.downcase
+    case user_command
+        when "find"
+        name = get_user_input
+        character = Character.find_by(name: name)
+        p character
+        when "add"
+        name = get_user_input
+        user.add_character_to_favorites(name)
+        when "delete"
+        name = get_user_input
+        user.delete_from_favorites(name)
+        when "rate"
+        name = get_user_input
+        puts "Enter a rating for this character"
+        rating = gets.chomp
+        user.update_character_rating(name, rating)
+        when "favorites"
+        user.favorite_characters_names  
+        when "strongest"
+        p user.strongest
 
-         when "smartest"
-            p user.smartest
-            
-         when "fastest"
-            p user.fastest
+        when "smartest"
+        p user.smartest
+        
+        when "fastest"
+        p user.fastest
 
+        when "exit"
+            exit_app
         end
- 
-     
- end
+
+    end
+
+    
+end
  
  runner
